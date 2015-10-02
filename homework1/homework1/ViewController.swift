80455b5ef625a3989c2b08feb77c71b4f174574f
Weschler, Sabrina <weschler@uw.edu>
2015-10-01 19:00:53 -0700

    Final hello world homework 1.

---
+++ homework1/homework1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by Weschler, Sabrina on 10/1/15.
+//  Copyright (c) 2015 Weschler, Sabrina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
